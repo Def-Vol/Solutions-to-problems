828bd0f89f9c87b0397132970ddadf48d8965670
Def Vol <ed8fed@gmail.com>
2026-09-09 08:58:03 +0800

    Update 26.Remove Duplicates from Sorted Array LC.py

--- Решения без разъяснений/26. Remove Duplicates from Sorted Array/26.Remove Duplicates from Sorted Array LC.py
+++ Решения без разъяснений/26. Remove Duplicates from Sorted Array/26.Remove Duplicates from Sorted Array LC.py
@@ -28,3 +28,25 @@ def my_solution2(nums):
     while ind < len(nums):
         del nums[ind+1:ind+nums.count(nums[ind])]
         ind += 1
+
+
+
+
+
+#Далее разбор решения сообщества
+
+#Лучшее решение сообщества предлагает замену первых элементов на уникальные, оставляя остальной 
+#список неизменным, ибо проверка допускает такое решение при возврате числа уникальных элементов
+def com_solution1(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if not nums:
+        return 0
+    res = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[res] = nums[i]
+            res += 1
+    return res
